4b652c8e41548ed4010fba42081833548e7cb0f8
Marcus Mörck <marcus.morck@gmail.com>
2017-11-09 10:50:53 +0100

    ovn2 completed

---
+++ form.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>form</title>
+    </head>
+    <body>
+        <form action="insert.php" method="post">
+            <fieldset>
+                <legend>
+                    <input type="text" placeholder="text" name="username">
+                    <input type="password" placeholder="text" name="password">
+                    <input type="submit" value="submit">
+                </legend>
+            </fieldset>
+        </form>
+    </body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcus
+ * Date: 2017-11-09
+ * Time: 09:28
+ */
